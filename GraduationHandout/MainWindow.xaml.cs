5956baebad714dbd218e72ee052117ac43fcbf6f
Cooper Duncan <cooper.m.duncan@ou.edu>
2021-09-16 19:56:44 -0500

    Graduation form complete B)

--- GraduationHandout/MainWindow.xaml.cs
+++ GraduationHandout/MainWindow.xaml.cs
@@ -24,5 +24,78 @@ namespace GraduationHandout
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            bool check = true;
+
+            if(string.IsNullOrWhiteSpace(citytxt.Text) == true)
+            {
+                check = false;
+                MessageBox.Show("Please enter a valid city.");
+            }
+            if (string.IsNullOrWhiteSpace(fntxt.Text) == true)
+            {
+                check = false;
+                MessageBox.Show("Please your first name.");
+            }
+            if (string.IsNullOrWhiteSpace(lntxt.Text) == true)
+            {
+                check = false;
+                MessageBox.Show("Please your last name.");
+            }
+            if (string.IsNullOrWhiteSpace(majortxt.Text) == true)
+            {
+                check = false;
+                MessageBox.Show("Please enter a valid major.");
+            }
+            double gpa;
+            if (double.TryParse(gpatxt.Text, out gpa) == false)
+            {
+                check = false;
+                MessageBox.Show("Please enter a valid GPA.");
+            }
+            if (string.IsNullOrWhiteSpace(statetxt.Text) == true)
+            {
+                check = false;
+                MessageBox.Show("Please enter a valid state.");
+            }
+            if (string.IsNullOrWhiteSpace(snametxt.Text) == true)
+            {
+                check = false;
+                MessageBox.Show("Please enter a valid street name.");
+            }
+
+
+            int streetnumber, zipcode;
+            if (int.TryParse(snumtxt.Text, out streetnumber) == false)
+            {
+                check = false;
+                MessageBox.Show("Please enter a valid GPA.");
+            }
+            if (int.TryParse(ziptxt.Text, out zipcode) == false)
+            {
+                check = false;
+                MessageBox.Show("Please enter a valid GPA.");
+            }
+
+            if (check == false)
+            {
+                return;
+            }
+
+            Student student = new Student()
+            {
+                FirstName = fntxt.Text,
+                GPA = gpa,
+                LastName = lntxt.Text,
+                Major = majortxt.Text
+            };
+            student.SetAddress(streetnumber, snumtxt.Text, statetxt.Text, citytxt.Text, zipcode);
+
+            Studinfo.Items.Add(student);
+
+        }
     }
 }
---
+++ GraduationHandout/Student.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GraduationHandout
+{
+    public class Student
+    {
+        public string     FirstName{get; set;}
+        public     string LastName {get; set;}
+        public     String Major    {get; set;}
+        public     double GPA      {get; set;}
+        public Address Address { get; set; }
+
+
+        public Student()
+        {
+            FirstName = string.Empty;
+            LastName = string.Empty;
+            Major = string.Empty;
+            GPA = 0;
+           Address = new Address(); 
+        }
+
+        public Student (string firstname, string lastname, string major, double gpa)
+        {
+            FirstName = firstname;
+            LastName = lastname;
+            Major = major;
+            GPA = gpa;
+            Address = new Address();
+        }
+
+
+        public string CalculateDist()
+        {
+            string dist;
+            if (GPA > 3.80)
+            {
+                dist = "Summa Cum Laude";
+            }
+            
+                else if (GPA >= 3.60)
+                {
+                dist = "Magna Cum Laude";
+            }
+            else if (GPA >= 3.40)
+            {
+                dist = "Cum Laude";
+            }
+            else
+            {
+                dist = " ";
+            }
+
+            return dist;
+        }
+
+        public void SetAddress (int streetnumber, string streetname, string state, string city, int zipcode)
+        {
+            Address = new Address(streetnumber, streetname, state, city, zipcode);
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName}, {LastName}, {Major}, {CalculateDist()}";
+        }
+    }
+}
